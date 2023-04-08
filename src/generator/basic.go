47c8965b473baf21463c9537efa4702d33d68e53
Daniil Solynin <didhat82@gmail.com>
2023-04-08 17:58:10 +0300

    add basic test data generator

---
+++ src/generator/basic.go
@@ -0,0 +1,24 @@
+package generator
+
+import (
+	"lab2/src/structs"
+	"math/rand"
+)
+
+func GenerateRectangles(rectanglesNumber int) []structs.Rectangle {
+	rectangles := make([]structs.Rectangle, 0, rectanglesNumber)
+
+	for i := 1; i <= rectanglesNumber; i++ {
+		rectangles = append(rectangles, structs.NewRectangleFromPrimitives(10*i, 10*i, 10*(2*rectanglesNumber-1), 10*(2*rectanglesNumber-1)))
+	}
+	return rectangles
+}
+
+func GeneratePoints(pointsNumber, minX, maxX, minY, maxY int) []structs.Point {
+	points := make([]structs.Point, 0, pointsNumber)
+
+	for i := 0; i < pointsNumber; i++ {
+		points = append(points, structs.NewPoint(rand.Intn(maxX-minX)+minX, rand.Intn(maxY-minY)+minY))
+	}
+	return points
+}
